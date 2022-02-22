e1cafce5914796defc08351139a364c0f816ad0a
Samuel Howard Lewis <samuel.lewis@astrazeneca.com>
2022-02-22 15:04:24 +0000

    Add Book class

---
+++ book.py
@@ -0,0 +1,11 @@
+class Book:
+    """A book"""
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return self.title + ' by ' + self.author
+
+book1 = Book('Hitchhikers', 'D Adams')
+print(book1)
