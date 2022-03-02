98f9eaba7b177fd356405a4228a9007f1117f5a4
ambevan <antonia.bevan@googlemail.com>
2022-03-02 14:14:44 +0000

    Delete book directory

--- book/book.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    """A book"""
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
-
-    def __str__(self):
-        return self.title + ' by ' + self.author
-
